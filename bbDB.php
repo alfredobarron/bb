ee843fb4d8be0ed5d7cf34f266f72e057cc3fd9c
Alfredo Barron <alfredobarronc@gmail.com>
2017-02-04 19:00:13 -0600

    Create bbDB.php

---
+++ bbDB.php
@@ -0,0 +1,44 @@
+<?php
+	 possesions
+	 	id
+	 	title
+	 	descriotion
+	 	tags [
+	 		//tag_ids
+	 		]
+	 	permissions [
+	 		//user_ids
+	 		]
+	 	date
+	 	isTrash bool
+	 	
+	 	
+	 // img, video, file
+	 files
+	 	id
+	 	user_id
+	 	name
+	 	title
+	 	description
+	 	
+	 tags
+	 	id
+	 	title
+	 	
+	 users
+	 	id
+	 	firstName
+	 	lastname
+	 	birthday
+	 	ine
+	 	curp
+	 	cedula
+	 	imss
+	 	avatar
+	 	conections[
+	 		//user_ids
+	 	]
+	 	
+	 
+ ?>
+    
